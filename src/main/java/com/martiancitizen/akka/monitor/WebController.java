9b656c62de6c07e6cb831cf518af90989e43acea
John Chamberlain <MartianCitizen@users.noreply.github.com>
2015-12-03 18:41:36 -0500

    Create WebController.java

---
+++ src/main/java/com/martiancitizen/akka/monitor/WebController.java
@@ -0,0 +1,26 @@
+package com.martiancitizen.akka.monitor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/status", method = RequestMethod.GET)
+    public String status(@RequestParam(value = "env", defaultValue = "dev") String env, Model model) {
+        String environment = env.isEmpty() ? WebApplication.environment : env;
+        Status status = new Status(env, Optional.of(model));
+
+        return "status";
+    }
+
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public String ping(Model model) {
+        return "ping";
+    }
+}
