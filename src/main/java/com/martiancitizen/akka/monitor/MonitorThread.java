a4a8fd6606336c7487d2595f3cfda257509dcf33
John Chamberlain <MartianCitizen@users.noreply.github.com>
2015-12-03 18:40:07 -0500

    Create MonitorThread.java

---
+++ src/main/java/com/martiancitizen/akka/monitor/MonitorThread.java
@@ -0,0 +1,28 @@
+package com.martiancitizen.akka.monitor;
+
+import org.springframework.ui.Model;
+
+import java.util.Optional;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.*;
+
+/**
+ * Created by johnchamberlain on 3/3/15.
+ */
+public class MonitorThread {
+
+    private final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+
+    public void scheduleStatusCheck() {
+        WebApplication.log(false, "Monitor thread starting");
+        final Runnable requestStatus = () -> {
+            WebApplication.log(false, "Getting status");
+            new Status(WebApplication.environment, Optional.<Model>empty());
+        };
+       scheduler.scheduleAtFixedRate(requestStatus, 1, 1, MINUTES);
+    }
+
+}
